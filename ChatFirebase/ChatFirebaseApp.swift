5f466e31027b9632779c845a8a078f69bfe39c7b
Rion <rionfx.42@gmail.com>
2021-10-10 08:01:15 +0900

    Created Project

---
+++ ChatFirebase/ChatFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatFirebaseApp.swift
+//  ChatFirebase
+//
+//  Created by FX on 2021/10/10.
+//
+
+import SwiftUI
+
+@main
+struct ChatFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
